6f0675d428ac56c60f8e54b7fd88a2793a8895c1
Roy Barneveld <roy.barneveld1@gmail.com>
2024-03-07 12:07:12 -0500

    Revert "Add simple version of splash effect" (Change of concept)
    This reverts commit 456919867d2b1794df5b61b326f97b4b37f7c1fc.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,10 @@
 import TypewriterTitle from "@/components/TypewriterTitle";
-import AnimatedInkSplashEffect from "@/components/AnimatedInkSplashEffect.tsx";
-
+import {motion} from "framer-motion";
+import Particles from "react-tsparticles";
 
 export default function Hero() {
     return (
         <div className="relative flex flex-col items-center justify-center h-[10rem]">
-            <AnimatedInkSplashEffect className="absolute -z-10 w-full h-full flex items-center justify-center"/>
             <TypewriterTitle/>
         </div>
     );
